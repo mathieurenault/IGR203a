524e33e24ebd1ea38de4dbc9feb7221001f601a9
Mathieu Renault <mathieu.renault@telecom-paristech.fr>
2016-04-13 12:14:31 +0200

    Squelette de l'application avec Action Bar Tabs et Fragments

---
+++ app/src/main/java/igr203a/carteinteractive/CarteFragment.java
@@ -0,0 +1,34 @@
+package igr203a.carteinteractive;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by Mathieu on 04/04/2016.
+ */
+
+
+public class CarteFragment extends Fragment {
+
+    public CarteFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.carte_fragment, container, false);
+    }
+
+}
