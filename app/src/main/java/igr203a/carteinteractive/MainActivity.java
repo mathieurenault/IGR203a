faf7b0081d1651db484a5aa40260b527a970fc8d
Mathieu Renault <mathieu.renault@telecom-paristech.fr>
2016-04-20 16:07:03 +0200

    Résolution des conflits et barre de titre masquée

--- app/src/main/java/igr203a/carteinteractive/MainActivity.java
+++ app/src/main/java/igr203a/carteinteractive/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements CarteFragment.Fra
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setVisibility(View.GONE); //On cache le titre de l'application
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
