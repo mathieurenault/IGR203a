c6cad4d68b2b566ee07d6241d8a44488248da0a4
Mathieu Renault <mathieu.renault@telecom-paristech.fr>
2016-04-20 14:10:54 +0200

    Bouton settings retiré du menu

--- app/src/main/java/igr203a/carteinteractive/MainActivity.java
+++ app/src/main/java/igr203a/carteinteractive/MainActivity.java
@@ -69,11 +69,6 @@ public class MainActivity extends AppCompatActivity implements CarteFragment.Fra
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
